53089a24bad3d4ab6a6aaf1cb4c25a408b27281e
munzii <munzii422@gmail.com>
2022-06-29 22:08:47 +0900

    12906

---
+++ Level1/12906.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        elif arr[i] != arr[i-1]:
+            answer.append(arr[i])
+    return answer
+
+print(solution([1,1,3,3,0,1,1]))
