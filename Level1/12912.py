913adbd7cacf82b3a4677573c096474e024619a0
munzii <munzii422@gmail.com>
2022-07-01 22:01:27 +0900

    12912

---
+++ Level1/12912.py
@@ -0,0 +1,14 @@
+def solution(a, b):
+    sum = 0
+    if a == b:
+        return a
+    elif a>b:
+        for i in range(b, a+1):
+            sum += i
+        return sum
+    else:
+        for i in range(a, b+1):
+            sum += i
+        return sum
+    
+print(solution(3, 5))
